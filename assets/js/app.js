93d725d9ee3b57f0efc8a7975e5b4397575a1085
ksbains <k1bains@ucsd.edu>
2017-09-22 15:44:47 -0700

    got some divs in but need to meet the requirments

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBSLUepk6oQpKVCkQj-p-Cwa4E057En3dI",
+      authDomain: "rock-paper-scissors-a3748.firebaseapp.com",
+      databaseURL: "https://rock-paper-scissors-a3748.firebaseio.com",
+      projectId: "rock-paper-scissors-a3748",
+      storageBucket: "rock-paper-scissors-a3748.appspot.com",
+      messagingSenderId: "540223845963"
+    };
+    firebase.initializeApp(config);
+    var db = firebase.database();
+
