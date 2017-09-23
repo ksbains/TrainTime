8456012e10870a588e555e3c3a8a620f5fd13309
ksbains <k1bains@ucsd.edu>
2017-09-22 18:14:00 -0700

    got the form to work, now need to add the moment.js part

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,15 @@
-
+function train (name, dest, time, freq){ 
+  this.name = name;
+  this.dest = dest;
+  this.time = time;
+  this.freq = freq;
+  this.changeName = function(name){
+    this.name = name;
+  }
+  this.getName = function(){
+    return this.name;
+  }
+}
 // Initialize Firebase
     var config = {
       apiKey: "AIzaSyBSLUepk6oQpKVCkQj-p-Cwa4E057En3dI",
@@ -10,4 +21,48 @@
     };
     firebase.initializeApp(config);
     var db = firebase.database();
+   $("#add-train").on("click", function(event) {
+      // prevent page from refreshing when form tries to submit itself
+      event.preventDefault();
+     // Capture user inputs and store them into variables
+      var name = $("#name-input").val().trim();
+      var dest = $("#dest-input").val().trim();
+      var time = $("#time-input").val().trim();
+      var freq = $("#freq-input").val().trim();
 
+     // Console log each of the user inputs to confirm we are receiving them
+      console.log(name);
+      console.log(dest);
+      console.log(time);
+      console.log(freq);      
+      /*<tr>
+          <td>BBB Express</td>
+          <td>San Jose</td>
+          <td>5</td>
+          <td>08:55PM</td>
+          <td>20</td>
+       </tr>*/
+
+       //make html ready elements with proper info
+      var tableRow = $("<tr></tr>");
+      var nameTd = $("<td>" + name +"</td>");
+      var destTd = $("<td>" + dest +"</td>");
+      var timeTd = $("<td>" + time +"</td>");
+      var freqTd = $("<td>" + freq +"</td>");
+
+      //append the html elements
+      tableRow.append(nameTd);
+      tableRow.append(destTd);
+      tableRow.append(timeTd);
+      tableRow.append(freqTd);
+      $("#tableBody").append(tableRow);
+
+      //push to firebase
+      var ref = db.ref("/Train");   
+      var refKey = ref.push({
+        name: name,
+        dest: dest,
+        time: time,
+        freq: freq
+      }).key;
+    });
